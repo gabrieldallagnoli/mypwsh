740f071be6fc9f5793f0914acf29cdd2ea928552
Gabriel Dallagnoli <187823668+gabrieldallagnoli@users.noreply.github.com>
2025-06-29 23:42:19 -0300

    Update profile URL to reflect correct repository for automatic updates

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
-# ---------------- v1.01 — 29/06/2025 -------------------
-# https://github.com/gabrieldallagnoli/powershell-profile
+# ----------- v1.01 — 29/06/2025 ------------
+# https://github.com/gabrieldallagnoli/mypwsh
 
 # =============================================
 # =============== Parâmetros ==================
@@ -32,7 +32,7 @@ $autoUpdateLog = [Environment]::GetFolderPath("MyDocuments") + "\PowerShell\Last
 function Update-Profile {
     try {
         Write-Host "Verificando atualizações do perfil..." -ForegroundColor Cyan
-        $url = "https://raw.githubusercontent.com/gabrieldallagnoli/powershell-profile/main/Microsoft.PowerShell_profile.ps1"
+        $url = "https://raw.githubusercontent.com/gabrieldallagnoli/mypwsh/main/Microsoft.PowerShell_profile.ps1"
         $oldhash = Get-FileHash $PROFILE
         Invoke-RestMethod $url -OutFile "$env:temp/Microsoft.PowerShell_profile.ps1"
         $newhash = Get-FileHash "$env:temp/Microsoft.PowerShell_profile.ps1"
